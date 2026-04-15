0e9c264825e4c0106f5ba7767e086f3baf809ed3
Paul Naber <paul.naber@gmx.de>
2026-04-15 22:23:30 +0200

    filter logs for sensitive keys

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"net/url"
 	"os"
 	"os/signal"
+	"slices"
 	"syscall"
 	"time"
 
@@ -148,7 +150,23 @@ func errorAttrs(err error) []slog.Attr {
 	return attrs
 }
 
+var sensitiveKeys = []string{"password", "key", "apikey", "secret", "pin", "creditcardno", "user"}
+
 func replaceAttr(groups []string, a slog.Attr) slog.Attr {
+	if slices.Contains(sensitiveKeys, a.Key) {
+		return slog.String(a.Key, "[REDACTED]")
+	}
+	if a.Value.String() != "" {
+		parsed, err := url.Parse(a.Value.String())
+		if err == nil && parsed.User != nil {
+			pw, _ := parsed.User.Password()
+			if pw != "" {
+				username := parsed.User.Username()
+				parsed.User = url.UserPassword(username, "[REDACTED]")
+				return slog.String(a.Key, parsed.String())
+			}
+		}
+	}
 	if a.Key == "error" {
 		err, ok := a.Value.Any().(error)
 		if !ok {
